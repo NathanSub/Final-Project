7ad6412b1b430a05a4b0d4ab498a63fa521bdb20
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-12-18 12:59:46 -0500

    Updated from Brython Server: 12/18/2018 12:59:46 PM

--- finalproject.py
+++ finalproject.py
@@ -5,4 +5,17 @@ Sources:
 
 '''
 
-print("Hello, world!")
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+from ggame import ImageAsset, Frame, Sound, SoundAsset, TextAsset
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+eye = Color(0xdee5ef, 1.0)
+sand = Color(0xefe49b, 1.0)
+meadowgreen = Color(0x8ed334, 1.0)
+orange = Color(0xe59e19, 1.0)
+
+thinline = LineStyle(1, black)
+noline = LineStyle(0,
