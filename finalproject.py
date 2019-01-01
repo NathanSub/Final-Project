ddef857150072fdf8a784512b549f7e82e504dab
NathanSub <42844708+NathanSub@users.noreply.github.com>
2019-01-01 14:19:14 -0500

    Updated from Brython Server: 1/1/2019 2:19:13 PM

--- finalproject.py
+++ finalproject.py
@@ -193,9 +193,9 @@ class Pong(App):
         
         self.ball = Ball((515, 265))
         
-        self.paddle1 = Paddle1((97, self.height - 250))
+        self.paddle1 = Paddle1((97, self.vy - 250))
         
-        self.paddle2 = Paddle2((935, self.height - 250))
+        self.paddle2 = Paddle2((935, self.vy - 250))
         
         self.borderleft = Borderleft((80, 265))
 
