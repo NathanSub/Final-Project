1b7dd038b7486c1e74a0e22cee2528ca8b6e7d96
NathanSub <42844708+NathanSub@users.noreply.github.com>
2019-01-02 12:16:56 -0500

    Updated from Brython Server: 1/2/2019 12:16:55 PM

--- finalproject.py
+++ finalproject.py
@@ -99,8 +99,8 @@ class Paddle2(Sprite):
     
     def __init__(self, position):
         super().__init__(Paddle2.paddle2, position)
-        self.vx = 1
-        self.vy = 1
+        self.vx = 0
+        self.vy = 0
         self.center = (0.5, 0.5)
         self.scale = 0.2
         
@@ -108,10 +108,10 @@ class Paddle2(Sprite):
         Pong.listenKeyEvent('keydown', "down arrow", self.downarrowKey)
         
     def uparrowKey(self, event):
-        self.vy+=-5
+        self.vy+=-1
         
     def downarrowKey(self, event):
-        self.vy+=5
+        self.vy+=1
 
     def step(self):
         self.y += self.vy
