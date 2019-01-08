57472c419d335e3ed9c4e7d4550cff38b83ec107
NathanSub <42844708+NathanSub@users.noreply.github.com>
2019-01-08 12:48:09 -0500

    Updated from Brython Server: 1/8/2019 12:48:09 PM

--- finalproject.py
+++ finalproject.py
@@ -32,7 +32,7 @@ whiteline = LineStyle(1, white)
 
 
 class Ball(Sprite):
-    ball = EllipseAsset(100, 100, whiteline, white)
+    ball = EllipseAsset(30, 30, whiteline, white)
     
     def __init__(self, position):
         super().__init__(Ball.ball, position)
